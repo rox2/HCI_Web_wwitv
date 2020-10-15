b0459ee30c24b1683d1fc86142d9b7034eb646bc
Dinushe <44283216+Dinushe@users.noreply.github.com>
2020-10-15 22:55:31 +0530

    deleted animation
    removed unnecessary animation

--- templateform.js
+++ templateform.js
@@ -127,19 +127,3 @@ function isURL(url) {
   }
 
   
-
-
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
-
-
